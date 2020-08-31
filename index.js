d66efb337c4d29ef82c4a495f7287ebf40b865ef
sara.mcguinn <sara.mcguinn@gmail.com>
2020-08-31 11:27:34 -0500

    Revised license list & corresponding badges/details

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-//objects for badges and license information
+//objects for badges and license information, from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+//which licenses to include was based on this list: https://www.synopsys.com/blogs/software-security/top-open-source-licenses/ 
 const licenseBadges = 
 {
 MIT : "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
 GNU_GPLv3: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
 Apache: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
 BSD_3clause: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-BSD_2clause: "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
 ISC: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
-Artistic_2: "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
-Unlicense: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+Artistic: "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
+GNU_LGPLv3: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
+Unlicense: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
 }
 
 const licenseDetails = 
@@ -23,10 +24,10 @@ MIT : "The MIT license is open source. For details, see https://opensource.org/l
 GNU_GPLv3: "The GNU General Public License is a free, copyleft license. For details, see https://www.gnu.org/licenses/gpl-3.0.",
 Apache: "The Apache 2.0 license is open source. For details, see https://opensource.org/licenses/Apache-2.0.",
 BSD_3clause: "The BSD 3-Clause license is open source. For details, see https://opensource.org/licenses/BSD-3-Clause.",
-BSD_2clause: "The BSD 2-Clause license is open source. For details, see https://opensource.org/licenses/BSD-2-Clause.",
 ISC: "The ISC license is open source. For details, see https://opensource.org/licenses/ISC.",
-Artistic_2: "The Artistic License 2.0, or Perl, is open source. For details, see https://opensource.org/licenses/Artistic-2.0.",
-Unlicense: "This code is public domain. For details, see http://unlicense.org/.",
+Artistic: "The Artistic License 2.0, or Perl, is open source. For details, see https://opensource.org/licenses/Artistic-2.0.",
+GNU_LGPLv3: "The GNU Lesser General Public License is a free, copyleft license. For details, see https://www.gnu.org/licenses/lgpl-3.0.",
+Unlicense: "This code is public domain. For details, see http://unlicense.org/."
 }
 
 //function to prompt user for input
@@ -76,7 +77,7 @@ function getUserInput() {
             type: "list",
             name: "license",
             message: "Project license:",
-            choices: ["MIT", "GNU_GPLv3", "Apache", "BSD_3clause", "BSD_2clause", "ISC", "Artistic_2", "Unlicense"]
+            choices: ["MIT", "GNU_GPLv3", "Apache", "BSD_3clause", "ISC", "Artistic", "GNU_LGPLv3", "Unlicense"]
         },
         {
             type: "input",
